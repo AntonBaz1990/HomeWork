f20f20eccb3f789921243c3a604c2b3b23190777
simpo5000 <simpo5000@gmail.com>
2022-10-23 00:27:43 +0300

    Добавил еще одно решение

--- Practik_03/Zadach_19/Program.cs
+++ Practik_03/Zadach_19/Program.cs
@@ -3,20 +3,45 @@
 // 14212 -> нет
 // 23432 -> да
 // 12821 -> да
+
+// Задача 19: Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
 Console.Clear();
+
+void Numb(int num)
+{   
+    int right = num / 10000;
+    int left = num % 10;
+    int right1 = num / 1000 % 10;
+    int left1 = num % 100 / 10;
+    if (num < 10000 || num > 99999) 
+    {
+        Console.WriteLine("Вы ввели не пятизначное число!!!");
+    }
+    else if (right == left && right1 == left1) 
+    {
+        Console.WriteLine("Да, это палиндромом");
+    }
+    else Console.WriteLine("НЕТ, это не палиндромом");
+}
 Console.WriteLine("Введите пятизначное число: ");
 int num = int.Parse(Console.ReadLine()!);
+Numb(num);
 
-int right = num / 10000;
-int left = num % 10;
-int right1 = num / 1000 % 10;
-int left1 = num % 100 / 10;
-if (num < 10000 || num > 99999)
-{
-    Console.WriteLine("Вы ввели не пятизначное число!!!");
-}
-else if (right == left && right1 == left1)
-{
-    Console.WriteLine("Да, это палиндромом");
-}
-else Console.WriteLine("НЕТ, это не палиндромом");
+// Альтернативный способ
+
+
+//Console.Clear();
+//Console.WriteLine("Введите пятизначное число: ");
+//int num = int.Parse(Console.ReadLine()!);
+//int right = num / 10000;
+//int left = num % 10;
+//int right1 = num / 1000 % 10;
+//int left1 = num % 100 / 10;
+//if (num < 10000 || num > 99999) Console.WriteLine("Вы ввели не пятизначное число!!!");
+//else if (right == left && right1 == left1) Console.WriteLine("Да, это палиндромом");
+//else Console.WriteLine("НЕТ, это не палиндромом");

1d0ca24fcb2ba6073370d949b23a5bccbc2a436d
simpo5000 <simpo5000@gmail.com>
2022-10-22 20:00:54 +0300

    add 3 practice

---
+++ Practik_03/Zadach_21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 3D пространстве.
+//  A (3,6,8); B (2,1,-7), -> 15.84
+//  A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+double AB(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double result =Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return result;
+}
+
+Console.WriteLine("Введите значение первой точки: Х1= ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение первой точки: Y1= ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение первой точки: Z1= ");
+double z1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение второй точки: Х2= ");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение второй точки: Y2= ");
+double y2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение второй точки: Z2= ");
+double z2 = double.Parse(Console.ReadLine()!);
+
+Console.Write($"Длина отрезка: {AB(x1, y1, z1, x2, y2, z2):f2}");
---
+++ Practik_03/Zadach_23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23: Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+string getNum(int n)
+{
+    string result = " ";
+    for (int i = 1; i < n; i++)
+    {
+        result += (Math.Pow(i, 3)).ToString()+", ";
+    }
+    result +=Math.Pow(n, 3).ToString();
+    return result;
+}
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(getNum(n));

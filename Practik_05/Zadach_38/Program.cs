2434da42b55ce81a754994169bc2c130b0ae33dc
simpo5000 <simpo5000@gmail.com>
2022-11-12 22:28:00 +0300

    Добавил ДЗ по семинару №5

---
+++ Practik_05/Zadach_38/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+void GetArray(double[] array)  // Функция рандомa массива
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] =  Convert.ToDouble(new Random().Next(100,1000)) / 100;
+    }
+}
+void PrintArray(int[] arr)  // Функция вывода массива
+{
+    int count = arr.Length;
+    for (int index = 0; index < count; index++)
+    {
+        Console.Write(arr[index] + " ");
+    }
+}
+
+double FindMin(double[] array)   // Функция нахождения минимального значения
+{
+    double countmin = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i]<countmin)
+        {
+            countmin= array[i];
+        }
+    }
+    return countmin;
+}
+double FindMax(double[] array) // Функция нахождения максимального значения
+{
+    double countmax = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>countmax)
+        {
+            countmax= array[i];
+        }
+    }
+    return countmax;
+}
+Console.Clear();
+
+double[] array = new double[10];
+GetArray(array);
+Console.WriteLine($"Массив вещественных чисел: [{String.Join(" ", array)}]");
+double countIndex1 = FindMin(array);
+Console.WriteLine($"Минимальная велечина в масиве: {countIndex1}");
+double countIndex2 = FindMax(array);
+Console.WriteLine($"Максимальная велечина в масиве: {countIndex2}");
+double n=FindMax(array)-FindMin(array);
+Console.WriteLine($"Pазницу между максимальным и минимальным элементов массива: {n}");
---
+++ Practik_05/Zadach_STARS/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача "со звездочкой": Разобраться с алгоритмом сортировки методом пузырька. Реализовать невозрастающую сторировку.
+// [3, 0, 2, 4, -1] -> [4, 3, 2, 0, -1]
+// [1,2,2,3,2] -> [3, 2, 2, 2, 1]
+
+int[] GetArray(int size, int minValue, int maxValue) // Создания рандомного массива;
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+void Selection(int[] array)                       // Создание функции сортировки массива;
+{
+    int length = array.Length;
+    for (int i = 0; i < length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+    }
+}
+Console.Clear();
+
+int[] array = GetArray(10,-10, 10);   //   Задаем условия массива;
+Console.WriteLine($"Вводный массив:         [{String.Join(", ", array)}]");
+Selection(array);
+Console.WriteLine($"Отсортированный массив: [{String.Join(", ", array)}]");
+
+
+
